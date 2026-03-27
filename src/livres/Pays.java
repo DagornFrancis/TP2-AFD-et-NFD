aaaf9427e38dcde06993db8a516a9a738d542adb
DagornFrancis <Alainfrancis08@yahoo.com>
2026-03-27 13:17:29 -0400

    Create Pays.java
    Ajout de la classe Pays

---
+++ src/livres/Pays.java
@@ -0,0 +1,30 @@
+package livres;
+
+public class Pays {
+
+        private String nom;
+        private String code;
+
+        public Pays(String nom, String code) {
+            this.nom = nom;
+            if (code != null && code.matches("^[A-Z]{3}$")) {
+                this.code = code;
+            } else {
+                throw new IllegalArgumentException("Le code pays doit contenir exactement 3 lettres majuscules.");
+            }
+        }
+
+        public String getNom() {
+            return nom;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        @Override
+        public String toString() {
+            return nom + " (" + code + ")";
+        }
+   
+}
