6a60d05c8953b2b30692ad84f21f5acd3f114f8d
FranckDilane237 <2434059@cegeplimoilou.ca>
2026-03-28 13:50:59 -0400

    Update OuvrageAudio.java
    Eupdate ouvrages audio

--- src/livres/OuvrageAudio.java
+++ src/livres/OuvrageAudio.java
@@ -4,33 +4,26 @@ import java.time.LocalDate;
 
 public class OuvrageAudio extends Ouvrage {
 
-    public enum FormatAudio { NUMERIQUE, ANALOGIQUE }
+    public enum FormatAudio {NUMERIQUE, ANALOGIQUE}
 
     private int dureeMinutes;
-
     private FormatAudio formatAudio;
 
     public OuvrageAudio(String titre, Auteur auteur, Format type, LocalDate date, int nombreExemplaires, int dureeMinutes, FormatAudio formatAudio) {
 
         super(titre, auteur, type, date, nombreExemplaires);
-
         this.dureeMinutes = dureeMinutes;
-
         this.formatAudio = formatAudio;
 
     }
 
     public int getDureeMinutes() {
-
         return dureeMinutes;
-
     }
 
     public FormatAudio getFormatAudio() {
-
         return formatAudio;
-
     }
 
 }
- 
+
